1ac43b49da4554ad00c3d3365561a38d0ee43403
Mohamad44p <mohamad44p@yahoo.com>
2025-02-08 13:04:37 +0200

    EDit font

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,14 +1,20 @@
 "use client"
 
+import { Montserrat } from "next/font/google"
 import LayoutAd from "@/components/admin/LayoutAd"
 import { Toaster } from "@/components/ui/toaster"
 import React from "react"
 
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+})
+
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className={`${montserrat.variable} font-sans`}>
       <LayoutAd>{children}</LayoutAd>
       <Toaster />
-    </>
+    </div>
   )
 }
